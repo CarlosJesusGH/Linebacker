fd6950a5a3497540f6073ef26c74dbd2d9facc05
Carlos Jesus <s.carlosj.28@gmail.com>
2015-11-29 19:23:40 -0430

    *Create Recording Bean. *Create RecordingDetailsActivity. *Set style to ReportButton

---
+++ app/src/main/java/com/cmsys/linebacker/util/LogUtils.java
@@ -0,0 +1,106 @@
+package com.cmsys.linebacker.util;
+
+/**
+ * Created by cj on 15/11/15.
+ *
+ * This is a set of methods used as help on logging tasks
+ * I started this from sample code published by Google.
+ */
+
+
+import android.util.Log;
+
+public class LogUtils {
+    private static final String LOG_PREFIX = "linebacker_";
+    private static final int LOG_PREFIX_LENGTH = LOG_PREFIX.length();
+    private static final int MAX_LOG_TAG_LENGTH = 23;
+
+    public static boolean LOGGING_ENABLED = true;
+
+    public static String makeLogTag(String str) {
+        if (str.length() > MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH) {
+            return LOG_PREFIX + str.substring(0, MAX_LOG_TAG_LENGTH - LOG_PREFIX_LENGTH - 1);
+        }
+
+        return LOG_PREFIX + str;
+    }
+
+    /**
+     * Don't use this when obfuscating class names!
+     */
+    public static String makeLogTag(Class cls) {
+        return makeLogTag(cls.getSimpleName());
+    }
+
+    public static void LOGD(final String tag, String message) {
+        if (LOGGING_ENABLED){
+            if (Log.isLoggable(tag, Log.DEBUG)) {
+                Log.d(tag, message);
+            }
+        }
+    }
+
+    public static void LOGD(final String tag, String message, Throwable cause) {
+        if (LOGGING_ENABLED){
+            if (Log.isLoggable(tag, Log.DEBUG)) {
+                Log.d(tag, message, cause);
+            }
+        }
+    }
+
+    public static void LOGV(final String tag, String message) {
+        if (LOGGING_ENABLED) {
+            if (Log.isLoggable(tag, Log.VERBOSE)) {
+                Log.v(tag, message);
+            }
+        }
+    }
+
+    public static void LOGV(final String tag, String message, Throwable cause) {
+        if (LOGGING_ENABLED) {
+            if (Log.isLoggable(tag, Log.VERBOSE)) {
+                Log.v(tag, message, cause);
+            }
+        }
+    }
+
+    public static void LOGI(final String tag, String message) {
+        if (LOGGING_ENABLED) {
+            Log.i(tag, message);
+        }
+    }
+
+    public static void LOGI(final String tag, String message, Throwable cause) {
+        if (LOGGING_ENABLED) {
+            Log.i(tag, message, cause);
+        }
+    }
+
+    public static void LOGW(final String tag, String message) {
+        if (LOGGING_ENABLED) {
+            Log.w(tag, message);
+        }
+    }
+
+    public static void LOGW(final String tag, String message, Throwable cause) {
+        if (LOGGING_ENABLED) {
+            Log.w(tag, message, cause);
+        }
+    }
+
+    public static void LOGE(final String tag, String message) {
+        if (LOGGING_ENABLED){
+            Log.e(tag, message);
+        }
+    }
+
+    public static void LOGE(final String tag, String message, Throwable cause) {
+        if (LOGGING_ENABLED) {
+            Log.e(tag, message, cause);
+        }
+    }
+
+    private LogUtils() {
+    }
+}
+
